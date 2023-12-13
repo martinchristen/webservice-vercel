9e257ddc78534d8d9b4bdd437d9d56aa9a09c88c
Martin Christen <martin.christen@fhnw.ch>
2023-12-13 11:51:42 +0100

    liste in API hinzugefügt

--- api/index.py
+++ api/index.py
@@ -6,3 +6,7 @@ app = FastAPI()
 def hello_world():
     return {"message": "Hello World",
             "version": "1.0"}
+
+@app.get("/api/list")
+def liste():
+    return {"liste": ["Apfel", "Banane", "Birne", "Ananas", "Mango", "Orange"]}
