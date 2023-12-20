6c43c61e41c44dd78818ba5c84588e20d9917e73
Martin Christen <martin.christen@fhnw.ch>
2023-12-20 09:50:45 +0100

    geolocation

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {AppBar, Toolbar, Button} from '@mui/material';
 import FruchtListe from './fruchtliste';
 import Addiere from './addiere';
-import Koordinaten from './koordinaten';
+import Koordinaten2 from './koordinaten2';
 
 export default function Index() {
     const [tab, setTab] = useState('tab1');
@@ -28,7 +28,7 @@ export default function Index() {
     {tab === 'tab1' && (<><h1>Inhalt Tab 1</h1></> )}
     {tab === 'tab2' && (<><h1><FruchtListe /></h1></> )}
     {tab === 'tab3' && (<><h1><Addiere /></h1></> )}
-    {tab === 'tab4' && (<><h1><Koordinaten /></h1></> )}
+    {tab === 'tab4' && (<><h1><Koordinaten2 /></h1></> )}
 
     </>)
 }
