84dd5f1e7a382f1125dfb3e923a3d181e6df7841
Martin Christen <martin.christen@fhnw.ch>
2023-12-20 09:18:24 +0100

    Create koordinaten2.tsx

---
+++ pages/koordinaten2.tsx
@@ -0,0 +1,87 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {Button, TextField, Typography, Paper} from '@mui/material';
+
+const Koordinaten2 = () => {
+    const [lng, setLng] = useState('0.0');
+    const [lat, setLat] = useState('0.0');
+    const [resultat, setResultat] = useState('');
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        // Überprüfen, ob die Geolocation-API verfügbar ist
+        if ("geolocation" in navigator) {
+
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const { latitude, longitude } = position.coords;
+              setLat(latitude.toString());
+              setLng(longitude.toString());
+            },
+            (error) => {
+              setError(`Fehler beim Abrufen der Position: ${error.message}`);
+            }
+          );
+        } else {
+          setError("Geolocation wird vom Browser nicht unterstützt");
+        }
+      }, []);
+
+    const addition = async() => {
+        try {
+            const response = await axios.get(`/api/transform?lat=${lat}&lng=${lng}`);
+            setResultat(response.data.result);
+        }
+        catch {
+            console.log("Fehler!! API Aufruf!!");
+        }
+    }
+
+    const readlng = (e) => {
+        const inputValue = e.target.value;
+        setLng(inputValue);
+      };
+
+      const readlat = (e) => {
+        const inputValue = e.target.value;
+        setLat(inputValue);
+        
+      };
+
+    return (
+    <>
+        <Paper elevation={5} style={{padding: '15px', margin: '15px', maxWidth: '400px'}}>
+            <Typography variant="h5">Transform WGS84 to LV95</Typography>
+            <TextField
+                label="Longitude"
+                type="text"
+                value={lng}
+                onChange={ (e) => readlng(e)}
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                label="Latitude"
+                type="text"
+                value={lat}
+                onChange={ (e) => readlat(e)}
+                fullWidth
+                margin="normal"
+            />
+            <Button variant="contained" color="primary" onClick={addition}>
+                Calculate
+            </Button>
+
+            {resultat !='' && (
+                <Typography variant="h6">
+                    Transformed: {resultat}
+                </Typography>
+
+            )}
+
+        </Paper>
+        
+    </>)
+}
+
+export default Koordinaten2;
